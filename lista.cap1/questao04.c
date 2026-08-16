a5ce447651da252644431cbac166695ad9ec95ce
Filipe <lippefalcaoo@gmail.com>
2026-08-16 02:34:07 -0300

    questao04 com comentarios agora

--- lista.cap1/questao04.c
+++ lista.cap1/questao04.c
@@ -11,12 +11,13 @@ return 0;
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>/*aqui ele usou um ';' onde nao é necessario aqui*/
 
-int main(){
-    int sem = 52;
-    printf("\nexistem %d semanas no ano\n", sem);
-    
+int main(){/*aqui ele colocou um 'M', porem o C é muito sensivel a essas mudanças,
+    alem que ele trocou a funçao das chaves{} com parenteses() */
+    int sem = 52;/*nesse aqui nem existe no original, mas deveria por causa do prinf*/
+    printf("\nexistem %d semanas no ano\n", sem);/*sem " " para fazer o texto*/
+    /*o comando que estaria qui foi retirado pois não existe no C, somente C++*/
     system("PAUSE");
     return 0;
 }
