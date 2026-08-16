df2802886cbed7189faa5f11bd52eeca4e0f5057
Filipe <lippefalcaoo@gmail.com>
2026-08-16 02:45:35 -0300

    questao04 atualizada para melhor visualização

--- lista.cap1/questao04.c
+++ lista.cap1/questao04.c
@@ -1,23 +1,28 @@
 /*
-#include <stdio.h>
-#include <stdlib.h>;
-int Main{}
-(
-printf( Existem %d semanas no ano.,52);
-cout << endl;
-system("PAUSE");
-return 0;
-)
+*#include <stdio.h>
+*#include <stdlib.h>; - aqui ele usou um ';' onde nao é necessario aqui -
+*
+*int Main{}  - aqui ele colocou um 'M', porem o C é muito sensivel a essas mudanças,
+*    alem que ele trocou a funçao das chaves{} com parenteses()-
+*(
+*- nesse aqui nem existe a declaraçao da variavel, mas deveria por causa do prinf
+*
+*printf( Existem %d semanas no ano.,52);  - sem " " para fazer o texto - 
+*
+*cout << endl; - o comando que estaria qui foi retirado pois não existe no C, somente C++ -
+*
+*system("PAUSE");
+*
+*return 0;
+*)
 */
 
 #include <stdio.h>
-#include <stdlib.h>/*aqui ele usou um ';' onde nao é necessario aqui*/
+#include <stdlib.h>
 
-int main(){/*aqui ele colocou um 'M', porem o C é muito sensivel a essas mudanças,
-    alem que ele trocou a funçao das chaves{} com parenteses() */
-    int sem = 52;/*nesse aqui nem existe no original, mas deveria por causa do prinf*/
-    printf("\nexistem %d semanas no ano\n", sem);/*sem " " para fazer o texto*/
-    /*o comando que estaria qui foi retirado pois não existe no C, somente C++*/
+int main(){
+    int sem = 52;
+    printf("\nexistem %d semanas no ano\n", sem);
     system("PAUSE");
     return 0;
 }
