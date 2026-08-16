9b7d754abebee572c7a80a1941ac41e681419fcd
Filipe <lippefalcaoo@gmail.com>
2026-08-16 02:25:34 -0300

    questao 04 respondida, falta comentarios para comparaçao

---
+++ lista.cap1/questao04.c
@@ -0,0 +1,22 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>;
+int Main{}
+(
+printf( Existem %d semanas no ano.,52);
+cout << endl;
+system("PAUSE");
+return 0;
+)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int sem = 52;
+    printf("\nexistem %d semanas no ano\n", sem);
+    
+    system("PAUSE");
+    return 0;
+}
