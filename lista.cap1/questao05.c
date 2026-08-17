0928446f9b2062f6dee8d8f308967eb9ccbe6f74
Filipe <lippefalcaoo@gmail.com>
2026-08-17 00:21:23 -0300

    questao05 realizada diretamente

---
+++ lista.cap1/questao05.c
@@ -0,0 +1,18 @@
+#include <stdio.h> /*faltava essas bibliotecas para funcionar*/
+#include <stdlib.h>
+
+int main() /*faltou int no main*/
+{
+printf("\nLinguagem C\n");
+system("pause");
+return 0; /*como igualmente faltou o return0*/
+}
+
+
+/* Original
+main()
+{
+printf("Linguagem C");
+system("pause");
+}
+*/
