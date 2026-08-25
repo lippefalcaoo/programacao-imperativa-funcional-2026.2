1ce666e7f9ad2e06f6bc6f86000c5f8bcab19e43
lippefalcaoo <lippefalcaoo@gmail.com>
2026-08-24 21:11:22 -0300

    dividindo questao 05 e .c e .md

--- lista.cap1/questao05/questao05.c
+++ lista.cap1/questao05/questao05.c
@@ -1,18 +1,9 @@
-#include <stdio.h> /* necessario para printf */
-#include <stdlib.h> /* necessario para system */
+#include <stdio.h> 
+#include <stdlib.h> 
 
-int main() /* define o tipo de retorno inteiro */
+int main() 
 {
 printf("\nLinguagem C\n");
 system("pause");
-return 0; /*indica finalizaçao bem sucedida*/
+return 0; 
 }
-
-
-/* Original
-main()
-{
-printf("Linguagem C");
-system("pause");
-}
-*/
