f2a0e29a519d9c1502605131a42246fc4d2b4c80
lippefalcaoo <lippefalcaoo@gmail.com>
2026-08-25 17:50:30 -0300

    questao 18 feita sem necessidade de .md

---
+++ lista.cap1/questao18/questao18.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float lapis = 4.88;
+    float borrachas = 234.54;
+    float canetas = 42.04;
+    float cadernos = 8.00;
+    float fitas = 13.05;
+
+    printf("\nLapis %12.2f",lapis);
+    printf("\nBorrachas %12.2f",borrachas);
+    printf("\nCanetas %12.2f",canetas);
+    printf("\nCadernos %12.2f",cadernos);
+    printf("\nFitas %12.2f",fitas);
+}
