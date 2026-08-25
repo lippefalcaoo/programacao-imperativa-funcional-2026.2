aa2e8b8ffbbcb0ec268686b0d2dd704903b55eee
lippefalcaoo <lippefalcaoo@gmail.com>
2026-08-24 21:28:51 -0300

    questao 07 feita e corrigida

---
+++ lista.cap1/questao07/questao07.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("\n\tBom dia! Shirley.");
+    printf("Você já tomou café? \n");
+    printf("\n\nA solução não existe!\nNão insista.");
+    printf("Duas\tlinhas\tde\tsaída\nou\tuma?");
+    printf("%s\n%s\n%s\n", "um", "dois", "três");
+    return 0;
+}
