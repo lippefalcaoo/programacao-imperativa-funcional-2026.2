1756a25914173fd3032efedcb0906ea47cff5cbd
lippefalcaoo <lippefalcaoo@gmail.com>
2026-08-25 20:50:06 -0300

    questao 27 feita

---
+++ lista.cap1/questao27/questao27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int total_segundos, horas, minutos, segundos;
+
+    printf("Digite o tempo em segundos: ");
+    scanf("%d", &total_segundos);
+
+    horas = total_segundos / 3600;
+    minutos = (total_segundos % 3600) / 60;
+    segundos = total_segundos % 60;
+
+    printf("\n%d segundos correspondem a %d hora(s), %d minuto(s) e %d segundo(s)", 
+           total_segundos, horas, minutos, segundos);
+
+    return 0;
+}
