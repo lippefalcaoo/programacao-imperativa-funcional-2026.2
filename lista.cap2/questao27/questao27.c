142622adbe99acef645ddfe0d1897e781813b8bc
lippefalcaoo <lippefalcaoo@gmail.com>
2026-09-16 19:18:48 -0300

    feita a questao 27

---
+++ lista.cap2/questao27/questao27.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+int main() {
+    srand(time(NULL));
+ 
+    int dado1 = rand() % 6 + 1;
+    int dado2 = rand() % 6 + 1;
+    int dado3 = rand() % 6 + 1;
+ 
+    printf("Dados: %d, %d, %d\n", dado1, dado2, dado3);
+ 
+    return 0;
+}
